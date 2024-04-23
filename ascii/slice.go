b344d7ca4473106802aebffd71b9ae3bd51b06f3
jkuya <kuyajesee@proton.me>
2024-04-23 19:42:18 +0300

    Handled \b and DEL

--- ascii/slice.go
+++ ascii/slice.go
@@ -6,17 +6,18 @@ import (
 )
 
 func Slice(word string) []string {
+	// Add a tab before the next word
 	re := regexp.MustCompile(`\\t`)
 	word = re.ReplaceAllString(word, "    $0")
 	word = re.ReplaceAllString(word, "")
-
+	// Add a tab and a new line before the next word
 	re1 := regexp.MustCompile(`\\v|\\f`)
 	word = re1.ReplaceAllString(word, "$0\\n    ")
 	word = re1.ReplaceAllString(word, "")
-
+	// Replace the previous words or characters
 	re2 := regexp.MustCompile(`(.*?)(\\r)`)
 	word = re2.ReplaceAllString(word, "")
-
+	// Delete a character before or after
 	re3 := regexp.MustCompile(`.\\b|DEL.`)
 	word = re3.ReplaceAllString(word, "")
 
